71d95fa9f864b976e580ad75c677b16d6d44ec1e
Cloudox <cloudox@foxmail.com>
2017-03-07 19:27:55 +0800

    实现获取属性、方法等列表；拦截方法调用；关联对象

--- RuntimeDemo/ViewController.m
+++ RuntimeDemo/ViewController.m
@@ -7,18 +7,84 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
 
-@interface ViewController ()
+@interface ViewController () <UIAlertViewDelegate>
+@property (nonatomic, strong) UIButton *logBtn;
 
+- (void)notHas;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 输出类的属性列表、方法列表、成员变量列表、协议列表
+    [self logInfo];
+    
+    // 调用不存在的方法
+    self.logBtn = [[UIButton alloc] initWithFrame:CGRectMake(100, 200, 100, 20)];
+    [self.logBtn setTitle:@"测 试" forState:UIControlStateNormal];
+    [self.logBtn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [self.logBtn addTarget:self action:@selector(notHas) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.logBtn];
+    
+    // 关联对象
+    static char associatedObjectKey;
+    objc_setAssociatedObject(self, &associatedObjectKey, @"我就是要关联的字符串对象内容", OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    NSString *theString = objc_getAssociatedObject(self, &associatedObjectKey);
+    NSLog(@"关联对象：%@", theString);
 }
 
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    NSLog(@"notFind!");
+    // 给本类动态添加一个方法
+    if ([NSStringFromSelector(sel) isEqualToString:@"notHas"]) {
+        class_addMethod(self, sel, (IMP)runAddMethod, "v@:*");
+    }
+    return YES;
+}
+
+// 要动态添加的方法
+ void runAddMethod(id self, SEL _cmd, NSString *string) {
+     NSLog(@"add IMP");
+}
+
+// 输出类的一些信息
+- (void)logInfo {
+    unsigned int count;
+    
+    // 获取属性列表
+    objc_property_t *propertyList = class_copyPropertyList([self class], &count);
+    for (unsigned int i = 0; i < count; i++) {
+        const char *propertyName = property_getName(propertyList[i]);
+        NSLog(@"property --> %@", [NSString stringWithUTF8String:propertyName]);
+    }
+    
+    // 获取方法列表
+    Method *methodList = class_copyMethodList([self class], &count);
+    for (unsigned int i; i < count; i++) {
+        Method method = methodList[i];
+        NSLog(@"method --> %@", NSStringFromSelector(method_getName(method)));
+    }
+    
+    // 获取成员变量列表
+    Ivar *ivarList = class_copyIvarList([self class], &count);
+    for (unsigned int i; i < count; i++) {
+        Ivar myIvar = ivarList[i];
+        const char *ivarName = ivar_getName(myIvar);
+        NSLog(@"Ivar --> %@", [NSString stringWithUTF8String:ivarName]);
+    }
+    
+    // 获取协议列表
+    __unsafe_unretained Protocol **protocolList = class_copyProtocolList([self class], &count);
+    for (unsigned int i; i < count; i++) {
+        Protocol *myProtocal = protocolList[i];
+        const char *protocolName = protocol_getName(myProtocal);
+        NSLog(@"protocol --> %@", [NSString stringWithUTF8String:protocolName]);
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
